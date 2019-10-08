c6b1e3eb8f489b24f821495d66fe4b1c8c918296
Franck Wehrling <franck.wehrling.external@spherea.com>
2019-10-08 13:10:35 +0200

    creation du pipe fromnow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { BooksComponent } from "./books/books.component";
 import { FruitComponent } from './fruit/fruit.component';
+import { FromNowPipe } from './pipes/from-now.pipe';
 
 @NgModule({
-  declarations: [AppComponent, BooksComponent, FruitComponent],
+  declarations: [AppComponent, BooksComponent, FruitComponent, FromNowPipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
